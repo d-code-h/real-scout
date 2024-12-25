e0c2f3705fcbd5a91ce924b244ab539a7bd6dd64
Yunus Habeeb <habeebdh1@gmail.com>
2024-12-25 06:28:34 +0100

    Add Appwrite integration with Google login and user management functions

---
+++ app/lib/appwrite.ts
@@ -0,0 +1,111 @@
+import { Client, Account, Avatars, OAuthProvider } from 'react-native-appwrite';
+import * as Linking from 'expo-linking';
+import { openAuthSessionAsync } from 'expo-web-browser';
+
+// Set the platform, endpoint, and project ID
+export const config = {
+  platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM,
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
+
+export const client = new Client();
+
+client
+  .setPlatform(config.endpoint!)
+  .setProject(config.projectId!)
+  .setPlatform(config.platform!);
+
+export const avatar = new Avatars(client);
+export const account = new Account(client);
+
+// Add a function to handle login
+export async function loginWithGoogle() {
+  try {
+    // Create a redirect URI
+    const redirecturi = Linking.createURL('/');
+
+    // Create an OAuth2 token
+    const response = await account.createOAuth2Token(
+      OAuthProvider.Google,
+      redirecturi,
+    );
+
+    // Throw an error if the response is empty
+    if (!response) throw new Error('Failed to login');
+
+    // Open the browser to authenticate the user
+    const browserResult = await openAuthSessionAsync(
+      response.toString(),
+      redirecturi,
+    );
+
+    // Throw an error if the browser result is empty
+    if (browserResult.type !== 'success') throw new Error('Failed to login');
+
+    // Parse the URL
+    const url = new URL(browserResult.url);
+
+    // Get the secret and user ID
+    const secret = url.searchParams.get('secret')?.toString();
+    const userId = url.searchParams.get('userId')?.toString();
+
+    // Throw an error if the secret or user ID is empty
+    if (!secret || !userId) throw new Error('Failed to login');
+
+    // Create a session
+    const session = await account.createSession(userId, secret);
+
+    // Throw an error if the session is empty
+    if (!session) throw new Error('Failed to create a session');
+
+    // Return true if the session is created
+    return true;
+  } catch (error) {
+    // Log the error
+    console.error(error);
+
+    // Return false if the session is not created
+    return false;
+  }
+}
+
+// Add a function to handle logout
+export async function logout() {
+  try {
+    // Delete the current session
+    await account.deleteSession('current');
+
+    // Return true if the session is
+    return true;
+  } catch (error) {
+    // Log the error
+    console.error(error);
+
+    // Return false if the session is not deleted
+    return false;
+  }
+}
+
+// Add a function to get user details
+export async function getUser() {
+  try {
+    // Get the user details
+    const user = await account.get();
+
+    // Return the user details
+    if (user.$id) {
+      // Get the user avatar
+      const userAvatar = await avatar.getInitials(user.name);
+
+      // Return the user details with the avatar
+      return { ...user, avatar: userAvatar.toString() };
+    }
+  } catch (error) {
+    // Log the error
+    console.error(error);
+
+    // Return null if the user details are not found
+    return null;
+  }
+}
